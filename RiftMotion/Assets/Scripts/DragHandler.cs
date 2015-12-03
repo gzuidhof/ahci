907205c489bad150d8b43046c17cadfea7ad0a03
Luc <l.nies@hotmail.com>
2015-12-03 10:16:10 +0100

    First dragging testing
    Doesn't work (yet)

---
+++ RiftMotion/Assets/Scripts/DragHandler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System;
+
+public class DragHandler : MonoBehaviour, IDragHandler, IPointerEnterHandler {
+
+
+    // Use this for initialization
+    void Start() {
+
+    }
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Dragging");
+    }
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Entering");
+    }
+}
