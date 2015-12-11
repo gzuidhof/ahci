cf2263929b9bedc78e982b2799ce9a9a46eca01b
Guido <me@guido.io>
2015-12-11 19:33:31 +0100

    Docs, fix postprocess swypehint shortening

--- swypy/swype2.py
+++ swypy/swype2.py
@@ -13,8 +13,8 @@ def prune_swipehint(query, swipehint):
     if len(query) > len(swipehint)+3:
         return True
     return False
-    
-    
+
+
 
 def edit_distance(word1, word2):
     return levenshtein(word1, word2)
@@ -25,21 +25,21 @@ def get_suggestions(query, n=5):
     for word in WORDS:
         if prune_word(query, word):
             continue
-        
+
         hint = sh.swipehint(word)
-        
+
         if prune_swipehint(query, hint):
             continue
-        
+
         distance = edit_distance(query, hint)
         results.append( (distance, word))
-        
+
     results = sorted(results,key=itemgetter(0))
-    
+
     return results[:n]
-    
-    
-    
+
+
+
 if __name__ == '__main__':
     test_cases = ['heqerqllo',               # hello
     'qwertyuihgfcvbnjk',                     # quick
@@ -50,7 +50,7 @@ if __name__ == '__main__':
     'mjuytfdsdftyuiuhgvc',                   # music
     'vghjioiuhgvcxsasdvbhuiklkjhgfdsaserty', # vocabulary
     ]
-        
-    for test in test_cases:
-        print test, get_suggestions(test)
-    
+
+    #for test in test_cases:
+        #print test, get_suggestions(test)
+    print get_suggestions('heqerqllo')
