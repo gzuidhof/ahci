c1f3106b3ad27380743f83710086ca4725742419
Guido <me@guido.io>
2016-01-04 05:44:05 +0100

    Print elapsed time on reques

--- swypy/api.py
+++ swypy/api.py
@@ -1,16 +1,21 @@
 from flask import Flask, jsonify, request
 import swype2 as swype
+import time
 
 app = Flask(__name__)
 
 @app.route('/suggest', methods=['GET'])
 def get_tasks():
     query = request.args.get('query', None)
-    print query;
+    print "QUERY:", query
     if query is not None and len(query) > 0:
+        query = str(query)
+        t = time.time()
         suggestions = swype.get_suggestions(query)
+        print "Elapsed time getting suggestions {:5.1f}ms".format((time.time()-t)*1000)
 
         suggestions = zip(*suggestions)[1] if len(suggestions) > 0 else []
+
         return jsonify({'query':query, 'suggestions':suggestions, 'error':'None'})
     else:
          return jsonify({'error':'No query argument given!', 'suggestions':[],query:None})
